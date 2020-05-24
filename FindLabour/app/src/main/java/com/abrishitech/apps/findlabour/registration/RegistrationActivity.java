313324ee04ec4d08e10ad474e23b22aac6f66b7f
AbdulBasitIQVIS <[abdulbasitrishi@gmail.com]>
2020-05-25 01:18:44 +0500

    Screenshots added.

--- FindLabour/app/src/main/java/com/abrishitech/apps/findlabour/registration/RegistrationActivity.java
+++ FindLabour/app/src/main/java/com/abrishitech/apps/findlabour/registration/RegistrationActivity.java
@@ -1,4 +1,16 @@
 package com.abrishitech.apps.findlabour.registration;
 
-public class RegistrationActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.abrishitech.apps.findlabour.R;
+
+public class RegistrationActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
